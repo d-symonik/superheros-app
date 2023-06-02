d7689f337d3bf54134fb1f723f252c7a92e7de17
Dmytro Symonik <79287440+d-symonik@users.noreply.github.com>
2023-06-02 12:12:35 +0300

    add a superhero model

---
+++ server/models/Superhero.js
@@ -0,0 +1,36 @@
+const sequelize = require('../db');
+
+const {DataTypes} = require('sequelize');
+const Superhero = sequelize.define('superhero', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nickname: {
+        type: DataTypes.STRING,
+        allowNull:false
+    },
+    real_name: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull:false
+    },
+    origin_description: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull:false
+    },
+    superpowers: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull:false
+    },
+    catch_phrase: {
+        type: DataTypes.STRING
+    },
+    images:{
+        type:DataTypes.ARRAY(DataTypes.STRING)
+    }
+});
+module.exports = {Superhero};
